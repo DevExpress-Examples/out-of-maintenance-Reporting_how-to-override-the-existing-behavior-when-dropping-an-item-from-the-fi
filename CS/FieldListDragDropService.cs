374233700a14d0c2b7fc8970afc25bdb20a9b751
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FieldListDragDropService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraReports.Design;
+using DevExpress.XtraReports.UI;
+
+namespace FieldListDragDrop {
+    public class MyFieldListDragDropService : IFieldListDragDropService {
+        IDesignerHost host;
+        public MyFieldListDragDropService(IDesignerHost host) { this.host = host; }
+        public IDragHandler GetDragHandler() {
+            return new MyFieldDragHandler(host);
+        }
+    }
+    public class MyFieldDragHandler : FieldDragHandler {
+        IDesignerHost mhost;
+        public MyFieldDragHandler(IDesignerHost host)
+            : base(host) {
+            this.mhost = host;
+        }
+        static bool MIsContolContainer(XRControl control) {
+            return control is Band || control is XRPanel;
+        }
+        public override void HandleDragDrop(object sender, DragEventArgs e) {
+            AdornerService.ResetSnapping();
+            RulerService.HideShadows();
+
+            XRControl parent = bandViewSvc.GetControlByScreenPoint(new Point(e.X, e.Y));
+            if (parent == null)
+                return;
+
+            XRControl control = (XRControl)Activator.CreateInstance(typeof(XRControl));
+
+            PointF basePoint = EvalBasePoint(e);
+            RectangleF controlRect = new RectangleF(basePoint, control.BoundsF.Size);
+            PointF dropPoint = bandViewSvc.PointToClientRelativeToBand(basePoint,
+                new DragDataObject(new XRControl[] { control }, control, 
+                    new RectangleF[] { controlRect }, controlRect));
+            XRRichText xRLabel = new XRRichText();
+
+            DesignTool.AddToContainer(host, xRLabel);
+
+            PointF location = PointToClient(xRLabel, dropPoint);
+
+            xRLabel.LocationF = location;
+            xRLabel.Size = new Size(100, 25);
+            xRLabel.DataBindings.Add("Rtf", null, "test");
+
+
+        }
+        static DragDataObject GetDragData(IDataObject dataObject) {
+            return (DragDataObject)dataObject.GetData(typeof(DragDataObject));
+        }
+        PointF PointToClient(XRControl control, PointF screenPoint) {
+            return ZoomService.GetInstance(host).
+                FromScaledPixels(bandViewSvc.PointToClient(screenPoint, null, control), ReportDpi);
+        }
+        protected float ReportDpi {
+            get {
+                return ((XtraReport)host.RootComponent).Dpi;
+            }
+        }
+    }
+}
---
+++ VB/FieldListDragDropService.vb
@@ -0,0 +1,73 @@
+﻿Imports System
+Imports System.ComponentModel.Design
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.XtraReports.Design
+Imports DevExpress.XtraReports.UI
+
+Namespace FieldListDragDrop
+    Public Class MyFieldListDragDropService
+        Implements IFieldListDragDropService
+        Private host As IDesignerHost
+        Public Sub New(ByVal host As IDesignerHost)
+            Me.host = host
+        End Sub
+        Public Function GetDragHandler() As IDragHandler _
+        Implements IFieldListDragDropService.GetDragHandler
+            Return New MyFieldDragHandler(host)
+        End Function
+    End Class
+
+    Public Class MyFieldDragHandler
+        Inherits FieldDragHandler
+        Private mhost As IDesignerHost
+        Public Sub New(ByVal host As IDesignerHost)
+            MyBase.New(host)
+            Me.mhost = host
+        End Sub
+        Private Shared Function MIsContolContainer(ByVal control As XRControl) As Boolean
+            Return TypeOf control Is Band OrElse TypeOf control Is XRPanel
+        End Function
+        Public Overrides Sub HandleDragDrop(ByVal sender As Object, ByVal e As DragEventArgs)
+            AdornerService.ResetSnapping()
+            RulerService.HideShadows()
+
+            Dim parent As XRControl = _
+                bandViewSvc.GetControlByScreenPoint(New System.Drawing.Point(e.X, e.Y))
+            If parent Is Nothing Then
+                Return
+            End If
+
+            Dim control As XRControl = CType(Activator.CreateInstance(GetType(XRControl)), XRControl)
+
+            Dim basePoint As PointF = EvalBasePoint(e)
+            Dim controlRect As New RectangleF(basePoint, control.BoundsF.Size)
+            Dim dropPoint As PointF = bandViewSvc.PointToClientRelativeToBand(basePoint, _
+                New DragDataObject(New XRControl() {control}, control, New RectangleF() _
+                                   {controlRect}, controlRect))
+            Dim xRLabel As New XRRichText()
+
+            DesignTool.AddToContainer(host, xRLabel)
+
+            Dim location As PointF = PointToClient(xRLabel, dropPoint)
+
+            xRLabel.LocationF = location
+            xRLabel.Size = New Size(100, 25)
+            xRLabel.DataBindings.Add("Rtf", Nothing, "test")
+
+
+        End Sub
+        Private Shared Function GetDragData(ByVal dataObject As IDataObject) As DragDataObject
+            Return CType(dataObject.GetData(GetType(DragDataObject)), DragDataObject)
+        End Function
+        Private Function PointToClient(ByVal control As XRControl, ByVal screenPoint As PointF) As PointF
+            Return ZoomService.GetInstance(host).FromScaledPixels(bandViewSvc.PointToClient(screenPoint, _
+                                                                                            Nothing, control), ReportDpi)
+        End Function
+        Protected ReadOnly Property ReportDpi() As Single
+            Get
+                Return (CType(host.RootComponent, XtraReport)).Dpi
+            End Get
+        End Property
+    End Class
+End Namespace
