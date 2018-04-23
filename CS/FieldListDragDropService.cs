1d17d76d868a566f4d110a083036c47c77092f25
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FieldListDragDropService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraReports.Design;
+using DevExpress.XtraReports.UI;
+
+namespace FieldListDragDrop {
+    public class MyFieldListDragDropService : IFieldListDragDropService {
+        IDesignerHost host;
+        public MyFieldListDragDropService(IDesignerHost host) { this.host = host; }
+        public IDragHandler GetDragHandler() {
+            return new MyFieldDragHandler(host);
+        }
+    }
+    public class MyFieldDragHandler : FieldDragHandler {
+        public MyFieldDragHandler(IDesignerHost host)
+            : base(host) {
+        }
+
+        public override void HandleDragDrop(object sender, DragEventArgs e) {
+            AdornerService.ResetSnapping();
+            RulerService.HideShadows();
+
+            XRControl parent = bandViewSvc.GetControlByScreenPoint(new Point(e.X, e.Y));
+            if (parent == null)
+                return;
+
+            XRRichText xRLabel = new XRRichText();
+            PointF location = GetDragDropLocation(e, xRLabel, parent);
+
+            DesignTool.AddToContainer(host, xRLabel);
+
+            xRLabel.LocationF = location;
+            xRLabel.Size = new Size(100, 25);
+            xRLabel.DataBindings.Add("Rtf", null, "test");
+        }
+
+        private PointF GetDragDropLocation(DragEventArgs e, XRControl control, XRControl parent) {
+            PointF bandPoint = EvalBandPoint(e, parent.Band);
+            bandPoint = bandViewSvc.SnapBandPoint(bandPoint, parent.Band, control, new XRControl[] { control });
+            PointF screenPoint = bandViewSvc.ControlViewToScreen(bandPoint, parent.Band);
+            return bandViewSvc.ScreenToControl(new RectangleF(screenPoint, SizeF.Empty), parent).Location;
+        }
+
+        static DragDataObject GetDragData(IDataObject dataObject) {
+            return (DragDataObject)dataObject.GetData(typeof(DragDataObject));
+        }
+    }
+}
---
+++ VB/XtraReport1.Designer.vb
@@ -0,0 +1,84 @@
+﻿Namespace FieldListDragDrop
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+			Me.nwindDataSet1 = New FieldListDragDrop.nwindDataSet()
+			Me.categoriesTableAdapter = New FieldListDragDrop.nwindDataSetTableAdapters.CategoriesTableAdapter()
+			DirectCast(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+			DirectCast(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.HeightF = 100F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' TopMargin
+			' 
+			Me.TopMargin.HeightF = 100F
+			Me.TopMargin.Name = "TopMargin"
+			Me.TopMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' BottomMargin
+			' 
+			Me.BottomMargin.HeightF = 100F
+			Me.BottomMargin.Name = "BottomMargin"
+			Me.BottomMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' nwindDataSet1
+			' 
+			Me.nwindDataSet1.DataSetName = "nwindDataSet"
+			Me.nwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+			' 
+			' categoriesTableAdapter
+			' 
+			Me.categoriesTableAdapter.ClearBeforeFill = True
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.TopMargin, Me.BottomMargin})
+			Me.DataAdapter = Me.categoriesTableAdapter
+			Me.DataMember = "Categories"
+			Me.DataSource = Me.nwindDataSet1
+			Me.Version = "9.3"
+			DirectCast(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+			DirectCast(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+		Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+		Private nwindDataSet1 As nwindDataSet
+		Private categoriesTableAdapter As FieldListDragDrop.nwindDataSetTableAdapters.CategoriesTableAdapter
+	End Class
+End Namespace
